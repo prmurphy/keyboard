4ce7bb2751062c1ca7cae4b630f85d246ed5920b
prmurphy <prmurphy27@gmail.com>
2022-01-20 19:56:17 -0600

    Create a React app and deploy it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect } from "react";
+import "./App.css";
+import AudioPlayer from "./AudioPlayer";
 function App() {
+  const audioPlayer = AudioPlayer();
+
+  useEffect(() => {
+    audioPlayer.setInstrument("acoustic_grand_piano");
+  }, []);
+
+  const handleClick = () => {
+    audioPlayer.playNote("C4");
+  };
+  const handleClick2 = () => {
+    audioPlayer.playNote("D4");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <button onClick={handleClick}>Play</button>
+      <button onClick={handleClick2}>Play</button>
     </div>
   );
 }
